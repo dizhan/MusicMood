43b46f8963c6d1ed4be9c50b77a3d3fbf592748f
Di Zhan <zhandi.happiness.1990@gmail.com>
2013-06-05 10:05:00 +0100

    Create class httpservlet

--- src/com/LMFM/musicmood/GuestbookServlet.java
+++ src/com/LMFM/musicmood/GuestbookServlet.java
@@ -8,22 +8,78 @@ import com.google.appengine.api.files.FileWriteChannel;
 import com.google.appengine.api.files.GSFileOptions.GSFileOptionsBuilder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
+import java.util.Iterator;
+import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
-
+@SuppressWarnings("serial")
 public class GuestbookServlet extends HttpServlet {
-	public static final String BUCKETNAME = "pipelinetest";
-	public static final String FILENAME = "eegtest";
-	
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		
-	}
+   public static final String BUCKETNAME = "YOUR_BUCKET_NAME";
+   public static final String FILENAME = "YOUR_FILE_NAME";
 
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("Hello, world from java");
+    FileService fileService = FileServiceFactory.getFileService();
+    GSFileOptionsBuilder optionsBuilder = new GSFileOptionsBuilder()
+       .setBucket(BUCKETNAME)
+       .setKey(FILENAME)
+       .setMimeType("text/html")
+       .setAcl("public-read")
+       .addUserMetadata("myfield1", "my field value");
+    AppEngineFile writableFile =
+         fileService.createNewGSFile(optionsBuilder.build());
+    // Open a channel to write to it
+     boolean lock = false;
+     FileWriteChannel writeChannel =
+         fileService.openWriteChannel(writableFile, lock);
+     // Different standard Java ways of writing to the channel
+     // are possible. Here we use a PrintWriter:
+     PrintWriter out = new PrintWriter(Channels.newWriter(writeChannel, "UTF8"));
+     out.println("The woods are lovely dark and deep.");
+     out.println("But I have promises to keep.");
+     // Close without finalizing and save the file path for writing later
+     out.close();
+     String path = writableFile.getFullPath();
+     // Write more to the file in a separate request:
+     writableFile = new AppEngineFile(path);
+     // Lock the file because we intend to finalize it and
+     // no one else should be able to edit it
+     lock = true;
+     writeChannel = fileService.openWriteChannel(writableFile, lock);
+     // This time we write to the channel directly
+     writeChannel.write(ByteBuffer.wrap
+               ("And miles to go before I sleep.".getBytes()));
+
+     // Now finalize
+     writeChannel.closeFinally();
+     resp.getWriter().println("Done writing...");
+
+     // At this point, the file is visible in App Engine as:
+     // "/gs/BUCKETNAME/FILENAME"
+     // and to anybody on the Internet through Cloud Storage as:
+     // (http://storage.googleapis.com/BUCKETNAME/FILENAME)
+     // We can now read the file through the API:
+     String filename = "/gs/" + BUCKETNAME + "/" + FILENAME;
+     AppEngineFile readableFile = new AppEngineFile(filename);
+     FileReadChannel readChannel =
+         fileService.openReadChannel(readableFile, false);
+     // Again, different standard Java ways of reading from the channel.
+     BufferedReader reader =
+             new BufferedReader(Channels.newReader(readChannel, "UTF8"));
+     String line = reader.readLine();
+     resp.getWriter().println("READ:" + line);
+
+    // line = "The woods are lovely, dark, and deep."
+     readChannel.close();
+  }
 }
